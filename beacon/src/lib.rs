d445b0b6031e86cb5edc19b2f7604cbcf928f767
VR314 <raghupathivaroon@gmail.com>
2024-10-20 20:34:31 -0400

    add beacon and beacon_tests as crates, start implementation skeleton

--- beacon/src/lib.rs
+++ beacon/src/lib.rs
@@ -1,14 +1,44 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+pub struct Client {
+    connection: ConnectionType,
+    // TODO: make an async function run() to actually run the client
+}
+
+pub struct Server {
+    connection: ConnectionType,
+    // TODO: make an async function run() to actually run the client
+}
+
+// This is used to abstract the client and server to use different types of connections in a
+// plug-and-play fashion.
+pub enum ConnectionType {
+    WebSocket(WebSocketConnection),
+    // IPC(IPCConnection),
+}
+
+pub trait Connection {
+    fn connect(&mut self);
+}
+
+pub struct WebSocketConnection {
+    address: String,
+    port: u32,
+    connected: bool,
+}
+
+impl Connection for WebSocketConnection {
+    fn connect(&mut self) {
+        println!("Connecting to {}:{}", self.address, self.port);
+        // TODO: connect to web socket
+        self.connected = true;
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    //    use super::*;
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        assert_eq!(2 + 2, 4);
     }
 }
